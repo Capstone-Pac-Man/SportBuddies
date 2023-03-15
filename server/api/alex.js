9b48925ae984b9eec58e0f628792dd64f2800595
Aliaksei Kalupaila <alexey2928@gmail.com>
2023-03-15 17:23:54 -0400

    alex routes

--- server/api/alex.js
+++ server/api/alex.js
@@ -1,1 +1,34 @@
 // Alex routes
+const router = require("express").Router();
+const User = require("../db/models/User")
+const UserSport = require("../db/models/UserSport")
+
+// POST api/users/
+router.post("/", async (req, res, next) => {
+	try {
+		const { name, email, mobile, password } = req.body;
+		const user = await User.create({
+			name: name,
+			email: email,
+			mobile : mobile,
+			password: password,
+		});
+		res.status(201).json(user);
+	} catch (error) {
+		next(error);
+	}
+});
+
+// api/users/:id
+router.get('/:id', async (req, res, next) => {
+	try {
+	  const user = await User.findByPk(req.params.id, {
+		include: [{
+			model:UserSport,
+		}],
+	  })
+	  res.json(user)
+	} catch (error) {
+	  next(error)
+	}
+})
