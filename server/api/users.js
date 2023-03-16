092ca0c565adc44f5c25044554c571d77cb73254
Karsten Fisk <karstenfisk7@gmail.com>
2023-03-16 15:07:20 -0700

    Added search

--- server/api/users.js
+++ server/api/users.js
@@ -1,5 +1,6 @@
 // Api Route
 const express = require("express");
+const { request } = require("http");
 const router = express.Router();
 const Sequelize = require("sequelize");
 const { User, UserSport, Sport } = require("../db/index");
@@ -54,12 +55,14 @@ router.put("/me", async (req, res, next) => {
     });
     if (sportId) {
       if (skillLevel) {
+        console.log("came here");
         await UserSport.update(
           { skillLevel: skillLevel },
           { where: { userId: id, sportId: sportId } }
         );
       }
       if (status) {
+        console.log("came here");
         await UserSport.update(
           { status: status },
           { where: { userId: id, sportId: sportId } }
@@ -69,8 +72,9 @@ router.put("/me", async (req, res, next) => {
     if (!user) {
       next();
     }
-    const updated = await user.update(rest);
-    res.json(updated);
+    await user.update(rest);
+    const final = await User.findByPk(id, { include: { model: Sport } });
+    res.json(final);
   } catch (e) {
     next(e);
   }
@@ -78,26 +82,34 @@ router.put("/me", async (req, res, next) => {
 
 router.get("/", async (req, res, next) => {
   try {
-    const params = req.params;
-    if (Object.keys(req.params).length === 0) {
+    const { filters } = req.query;
+    let filter = [];
+    if (filters) {
+      filter = JSON.parse(filters);
+      console.log(filter);
+    }
+    if (filter.length === 0) {
       const users = await User.findAll();
       res.json(users);
     } else {
       const userSports = await UserSport.findAll({
         where: {
           status: "active",
-          [Sequelize.Op.or]: params.map((e) => ({
-            sportId: e.sportId,
-            skillLevel: e.skillLevel,
+          [Sequelize.Op.or]: filter.map((val) => ({
+            sportId: val.sportId,
+            ...(val.skillLevel ? { skillLevel: val.skillLevel } : {}),
           })),
         },
       });
-      const ids = userSports.map((val) => {
+      const id = userSports.map((val) => {
         return val.userId;
       });
       const users = await User.findAll({
         where: {
-          ids,
+          id,
+        },
+        include: {
+          model: Sport,
         },
       });
       if (!users) {
@@ -111,14 +123,33 @@ router.get("/", async (req, res, next) => {
 });
 
 router.post("/me/sports", async (req, res, next) => {
-  const { sportId, skillLevel, userId } = req.body;
+  const { sportId, skillLevel, userId, status } = req.body;
   const user = await User.findByPk(userId);
   await UserSport.create({
     userId: userId,
     sportId: sportId,
     skillLevel: skillLevel,
+    status: status,
   });
   const updatedUser = await User.findByPk(user.id, {
+    include: {
+      model: Sport,
+    },
+  });
+
+  res.json(updatedUser);
+});
+
+router.delete("/me/sports", async (req, res, next) => {
+  const { sportId, userId } = req.body;
+  const userSport = await UserSport.findOne({
+    where: {
+      userId: userId,
+      sportId: sportId,
+    },
+  });
+  await userSport.destroy();
+  const updatedUser = await User.findByPk(userId, {
     include: {
       model: Sport,
     },
