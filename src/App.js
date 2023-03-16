8718e32bac92d1cdd52b36300eb719cc013fb981
kbwong3 <brianwong0109@gmail.com>
2023-03-16 17:39:10 -0400

    fixed logout button display on navbar issue

--- src/App.js
+++ src/App.js
@@ -1,47 +1,21 @@
 import "./index.css";
-import React, {useState, useEffect }  from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
 import Login from "./components/UserAuth/Login";
 import SignUp from "./components/UserAuth/SignUp";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./config/firebase"
 import NavBar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { HomePage } from "./components/homepage/homepage";
 
-
-
-
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  
-  useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setIsLoggedIn(true);
-      } else {
-        setIsLoggedIn(false);
-      }
-    });
-  }, [isLoggedIn]);
-  
-
   return (
     <div className="App">
-      <NavBar/>
-      {isLoggedIn ? (
-        <Routes>
-          {/* needs to be deleted after we have home page */}
-          <Route path="/" element={<HomePage />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      )}
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   );
 }
