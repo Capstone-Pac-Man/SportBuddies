5728e4cbd68e143540100076dc17965663701bbf
kbwong3 <brianwong0109@gmail.com>
2023-03-16 15:38:12 -0400

    created homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import "./index.css";
 import { Authenticate } from "./components/auth";
 import { Signup } from "./components/signup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { HomePage } from "./components/homepage/homepage";
+
 function App() {
   return (
     <div className="App">
-      Hello
       <Authenticate />
       <Signup />
+      <HomePage />
     </div>
   );
 }
