0cd0603d334e50417280e8d9c1ea1df5c60c3bb7
Karsten Fisk <karstenfisk7@gmail.com>
2023-03-27 14:18:49 -0700

    Edited venue dashboard auth

--- server/db/models/Venue.js
+++ server/db/models/Venue.js
@@ -2,7 +2,7 @@ const Sequelize = require("sequelize");
 const db = require("../db");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const Sport = require("./Sport")
+const Sport = require("./Sport");
 
 const Venue = db.define(
   "venue",
@@ -61,7 +61,7 @@ const Venue = db.define(
     },
     imageUrl: {
       type: Sequelize.STRING,
-      defaultValue: "https://cdn-icons-png.flaticon.com/512/2463/2463422.png"
+      defaultValue: "https://cdn-icons-png.flaticon.com/512/2463/2463422.png",
     },
     latitude: {
       type: Sequelize.FLOAT,
@@ -76,54 +76,52 @@ const Venue = db.define(
 const SALT_ROUNDS = 5;
 
 Venue.prototype.correctPassword = function (candidatePwd) {
-	//we need to compare the plain version to an encrypted version of the password
-	return bcrypt.compare(candidatePwd, this.password);
+  //we need to compare the plain version to an encrypted version of the password
+  return bcrypt.compare(candidatePwd, this.password);
 };
-  
-  Venue.prototype.generateToken = function () {
-	  return jwt.sign({ id: this.id }, process.env.JWT);
-  };
-  
- 
-  Venue.authenticate = async function ({ email, password }) {
-	const venue = await this.findOne({ where: { email } });
-	if (!venue || !(await venue.correctPassword(password))) {
-	  const error = Error("Incorrect email/password");
-	  error.status = 401;
-	  throw error;
-	}
-	return venue.generateToken();
-  };
-  
-  Venue.findByToken = async function (token) {
-	try {
-	  const { id } = await jwt.verify(token, process.env.JWT);
-	  const venue = Venue.findByPk(id, {
+
+Venue.prototype.generateToken = function () {
+  return jwt.sign({ id: this.id }, process.env.JWT);
+};
+
+Venue.authenticate = async function ({ email, password }) {
+  const venue = await this.findOne({ where: { email } });
+  if (!venue || !(await venue.correctPassword(password))) {
+    const error = Error("Incorrect email/password");
+    error.status = 401;
+    throw error;
+  }
+  return venue.generateToken();
+};
+
+Venue.findByToken = async function (token) {
+  try {
+    const { id } = await jwt.verify(token, process.env.JWT);
+    const venue = Venue.findByPk(id, {
       include: {
         model: Sport,
-      }
+      },
     });
-	  if (!venue) {
-		  throw "Venue doesn't Exist";
-	  }
-	  return venue;
-	} catch (ex) {
-	  const error = Error("bad token");
-	  error.status = 401;
-	  throw error;
-	}
-  };
-  
-  
-  const hashPassword = async (venue) => {
-	//in case the password has been changed, we want to encrypt it with bcrypt
-	if (venue.changed("password")) {
-	  venue.password = await bcrypt.hash(venue.password, SALT_ROUNDS);
-	}
-  };
-  
-  Venue.beforeCreate(hashPassword);
-  Venue.beforeUpdate(hashPassword);
-  Venue.beforeBulkCreate((venues) => Promise.all(venues.map(hashPassword)));
+    if (!venue) {
+      throw new Error("Venue doesn't Exist");
+    }
+    return venue;
+  } catch (ex) {
+    const error = Error("bad token");
+    error.status = 401;
+    throw error;
+  }
+};
+
+const hashPassword = async (venue) => {
+  //in case the password has been changed, we want to encrypt it with bcrypt
+  if (venue.changed("password")) {
+    venue.password = await bcrypt.hash(venue.password, SALT_ROUNDS);
+  }
+};
+
+Venue.beforeCreate(hashPassword);
+Venue.beforeUpdate(hashPassword);
+Venue.beforeBulkCreate((venues) => Promise.all(venues.map(hashPassword)));
 
 module.exports = Venue;
