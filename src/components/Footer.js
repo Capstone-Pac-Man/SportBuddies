0956af5e8605b4ae9b31778ddc12ec9b5e703ca2
Karsten Fisk <karstenfisk7@gmail.com>
2023-03-28 18:47:37 -0700

    added footer mobile functionality

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,76 +7,68 @@ import { FaSlack } from "react-icons/fa";
 import { fetchOneVenueAsync } from "../reducers/venueAuthSlice";
 
 function Footer() {
-	const isVenueLoggedIn = useSelector((state) => !!state.auth.id);
-	const dispatch = useDispatch();
-	useEffect(() => {
-		dispatch(fetchOneVenueAsync());
-	}, []);
+  const isVenueLoggedIn = useSelector((state) => !!state.auth.id);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchOneVenueAsync());
+  }, []);
 
-	return (
-		<Container
-			expand="lg"
-			className="bg-dark text-white"
-			id="footer"
-			fluid>
-			<Row>
-				<Col className="col-md-4 text-align-center">
-					<h4>About Us</h4>
-					<p>Awesome guys</p>
-				</Col>
-				<Col className="col-md-4">
-					<h4>Become our business partner</h4>
-					{isVenueLoggedIn ? (
-						<Link
-							to="/venue/dashboard"
-							className="link">
-							Visit your venue account
-						</Link>
-					) : (
-						<div className="d-flex">
-							<div>
-								<Link
-									to="/venue/signup"
-									className="link">
-									Create venue account
-								</Link>
-								<br></br>
-								<Link
-									to="/venue/login"
-									className="link">
-									Sign in to venue account
-								</Link>
-							</div>
-						</div>
-					)}
-				</Col>
-				<Col className="col-md-4">
-					<h4>Connect With Us</h4>
-					<Row>
-						<Link
-							to="https://github.com/Capstone-Pac-Man/Capstone.git"
-							className="link">
-							<AiFillGithub /> Our Github Project
-						</Link>
-					</Row>
-					<Row>
-						<Link
-							to="https://fullstackacademy.slack.com/archives/C04TT9NMZ6G"
-							className="link">
-							<FaSlack /> Our Slack
-						</Link>
-					</Row>
-				</Col>
-			</Row>
-			<br></br>
-			<Row>
-				<p class="text-center">
-					© 2023 All Rights Reserved. By Karsten Fisk, Aliaksei Kalupaila, Jason
-					Wang, Brian Wong.
-				</p>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container expand="lg" className="bg-dark text-white" id="footer" fluid>
+      <Row>
+        <Col className="col-md-4 text-align-center">
+          <h4>About Us</h4>
+          <p>Awesome guys</p>
+        </Col>
+        <Col className="col-md-4">
+          <h4>Become our business partner</h4>
+          {isVenueLoggedIn ? (
+            <Link to="/venue/dashboard" className="link">
+              Visit your venue account
+            </Link>
+          ) : (
+            <div className="d-flex">
+              <div>
+                <Link to="/venue/signup" className="link">
+                  Create venue account
+                </Link>
+                <br></br>
+                <Link to="/venue/login" className="link">
+                  Sign in to venue account
+                </Link>
+              </div>
+            </div>
+          )}
+        </Col>
+        <Col className="col-md-4">
+          <h4>Connect With Us</h4>
+          <Row>
+            <Link
+              to="https://github.com/Capstone-Pac-Man/Capstone.git"
+              className="link"
+            >
+              <AiFillGithub /> Our Github Project
+            </Link>
+          </Row>
+          <Row>
+            <Link
+              to="https://fullstackacademy.slack.com/archives/C04TT9NMZ6G"
+              className="link"
+            >
+              <FaSlack /> Our Slack
+            </Link>
+          </Row>
+        </Col>
+      </Row>
+      <br></br>
+      <Row>
+        <p class="text-center">
+          © 2023 All Rights Reserved. By Karsten Fisk, Aliaksei Kalupaila, Jason
+          Wang, Brian Wong.
+        </p>
+      </Row>
+    </Container>
+  );
 }
 
 export default Footer;
