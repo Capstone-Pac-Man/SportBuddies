2f8d950eb046cadd514bf5d9d33e278b0ada2e82
kbwong3 <brianwong0109@gmail.com>
2023-03-30 16:03:36 -0400

    finished chat styling

--- src/components/chat/Sidebar.js
+++ src/components/chat/Sidebar.js
@@ -1,7 +1,7 @@
 import { useSelector, useDispatch } from "react-redux";
 import { selectUser } from "../../reducers/userSlice";
-import React, { useState, useEffect } from "react";
-import { Tab, Nav } from "react-bootstrap";
+import React, { useState, useEffect, useCallback } from "react";
+import { Tab, Nav, Form, InputGroup, Button } from "react-bootstrap";
 import {
   selectConversations,
   fetchAllUserConversations,
@@ -15,7 +15,7 @@ import { fetchAllMessagesInConvo } from "../../reducers/messageSlice";
 import { fetchOneUserAsync } from "../../reducers/userSlice";
 // const socket = io.connect("http://localhost:5000");
 
-// const CONVERSATIONS_KEY = "conversations";
+const CONVERSATIONS_KEY = "conversations";
 
 export default function Sidebar() {
   const [selected, setSelected] = useState(null);
@@ -23,6 +23,11 @@ export default function Sidebar() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
   const conversations = useSelector((state) => state.conversations);
+  const setRef = useCallback((node) => {
+    if (node) {
+      node.scrollIntoView({ smooth: true });
+    }
+  }, []);
   useEffect(() => {
     dispatch(fetchOneUserAsync());
     dispatch(fetchAllUserConversations());
@@ -48,39 +53,127 @@ export default function Sidebar() {
       setContent("");
     }
   };
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    console.log("-->>", content);
+    const obj = {
+      id: selected,
+      content: content,
+    };
+    dispatch(updateSelectedConvo(obj));
+    setContent("");
+  }
+
   return (
-    <div>
-      {conversations.userConversations.map((e) => {
-        return (
-          <div
-            onClick={(e) => handleSelect(e)}
-            className={parseInt(selected) === e.id ? "display-6" : null}
-            key={e.id}
-            id={e.id}
-          >
-            {e.users[0].firstName} {e.users[0].lastName}
-          </div>
-        );
-      })}
-      {selected
-        ? conversations.singleConversation.conversationMessages.map((e) => {
-            return (
-              <div
-                style={
-                  e.senderId === user.id ? { backgroundColor: "blue" } : null
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <div
+        style={{ width: "250px", height: "90%" }}
+        className="d-flex flex-column">
+        <Tab.Container
+          activeKey={CONVERSATIONS_KEY}
+          onSelect={CONVERSATIONS_KEY}>
+          <Nav variant="tabs" className="justify-content-center">
+            <Nav.Item>
+              <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+            </Nav.Item>
+          </Nav>
+          <Tab.Content className="border overflow-auto flex-grow-1">
+            <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+              <ListGroup variant="flush">
+                {conversations.userConversations.map((e) => {
+                  return (
+                    <ListGroup.Item
+                      onClick={(e) => handleSelect(e)}
+                      action
+                      key={e.id}
+                      id={e.id}
+                      active={parseInt(selected) === e.id}>
+                      {e.users[0].firstName} {e.users[0].lastName} {e.id}
+                    </ListGroup.Item>
+                  );
+                })}
+              </ListGroup>
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
+      </div>
+
+      {selected ? (
+        <div
+          style={{
+            height: "90%",
+          }}
+          className="d-flex flex-column flex-grow-1">
+          <div className="flex-grow-1 overflow-auto">
+            <div className="d-flex flex-column align-items-start justify-content-end px-3">
+              {" "}
+              {conversations.singleConversation.conversationMessages.map(
+                (e) => {
+                  const lastMessage =
+                    conversations.singleConversation.length - 1 === e.id;
+
+                  const theSender = e.senderId === user.id;
+
+                  return (
+                    // <div
+                    //   style={
+                    //     e.senderId === user.id
+                    //       ? { backgroundColor: "blue" }
+                    //       : null
+                    //   }>
+                    //   {e.content}
+                    // </div>
+
+                    <div
+                      ref={lastMessage ? setRef : null}
+                      key={e.id}
+                      className={`my-1 d-flex flex-column ${
+                        theSender
+                          ? "align-self-end align-items-end"
+                          : "align-items-start"
+                      }`}>
+                      <div
+                        className={`rounded px-2 py-1 ${
+                          theSender ? "bg-primary text-white" : "border"
+                        }`}>
+                        {e.content}
+                      </div>
+                      <div
+                        className={`text-muted small ${
+                          theSender ? "text-right" : ""
+                        }`}>
+                        {theSender ? "You" : e.senderId}
+                      </div>
+                    </div>
+                  );
                 }
-              >
-                {e.content}
-              </div>
-            );
-          })
-        : null}
-      <input
+              )}
+            </div>
+          </div>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="m-2">
+              <InputGroup>
+                <Form.Control
+                  as="textarea"
+                  required
+                  value={content}
+                  onChange={(e) => setContent(e.target.value)}
+                  onKeyDown={(e) => handleSend(e)}
+                  style={{ height: "75px", resize: "none" }}
+                />
+                <Button type="submit">Send</Button>
+              </InputGroup>
+            </Form.Group>
+          </Form>
+        </div>
+      ) : null}
+      {/* <input
         type="text"
         value={content}
         onChange={(e) => setContent(e.target.value)}
-        onKeyDown={(e) => handleSend(e)}
-      ></input>
+        onKeyDown={(e) => handleSend(e)}></input> */}
     </div>
   );
 }
